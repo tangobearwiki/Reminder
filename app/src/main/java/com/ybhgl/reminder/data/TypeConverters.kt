095a8aef36f5688cfc11718e9ecc02824de6f301
Tangobear <tangobearwiki@gmail.com>
2026-08-15 23:50:36 +0800

    Add app/src/main/java/com/ybhgl/reminder/data/TypeConverters.kt

---
+++ app/src/main/java/com/ybhgl/reminder/data/TypeConverters.kt
@@ -0,0 +1,47 @@
+package com.ybhgl.reminder.data
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.json.Json
+import java.time.LocalDate
+
+class TypeConverters {
+    @TypeConverter
+    fun fromString(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun localDateToString(date: LocalDate?): String? {
+        return date?.toString()
+    }
+
+    @TypeConverter
+    fun toReminderType(value: String) = enumValueOf<ReminderType>(value)
+
+    @TypeConverter
+    fun fromReminderType(value: ReminderType) = value.name
+
+    @TypeConverter
+    fun fromRepeatInfo(repeatInfo: RepeatInfo?): String? {
+        return repeatInfo?.let { Json.encodeToString(it) }
+    }
+
+    @TypeConverter
+    fun toRepeatInfo(json: String?): RepeatInfo? {
+        return json?.let { Json.decodeFromString<RepeatInfo>(it) }
+    }
+
+    @TypeConverter
+    fun fromReminderNotificationConfig(config: ReminderNotificationConfig): String {
+        return Json.encodeToString(config)
+    }
+
+    @TypeConverter
+    fun toReminderNotificationConfig(json: String): ReminderNotificationConfig {
+        return try {
+            Json.decodeFromString<ReminderNotificationConfig>(json)
+        } catch (e: Exception) {
+            ReminderNotificationConfig()
+        }
+    }
+}
