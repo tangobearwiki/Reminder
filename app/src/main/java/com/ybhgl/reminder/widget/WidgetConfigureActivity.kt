8a6e52879d4024651db6a08db59d26155a1caa8d
Tangobear <tangobearwiki@gmail.com>
2026-08-16 00:49:05 +0800

    fix: add missing imports in WidgetConfigureActivity.kt

--- app/src/main/java/com/ybhgl/reminder/widget/WidgetConfigureActivity.kt
+++ app/src/main/java/com/ybhgl/reminder/widget/WidgetConfigureActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.SystemBarStyle
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
