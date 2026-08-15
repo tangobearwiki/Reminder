5f50e0fc441a7b4a3eda558b7a3ac386c2b69070
Tangobear <tangobearwiki@gmail.com>
2026-08-15 23:51:09 +0800

    Add app/src/main/java/com/ybhgl/reminder/ui/common/ViewModelProvider.kt

---
+++ app/src/main/java/com/ybhgl/reminder/ui/common/ViewModelProvider.kt
@@ -0,0 +1,84 @@
+package com.ybhgl.reminder.ui.common
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.ybhgl.reminder.ReminderApplication
+import com.ybhgl.reminder.ui.add.AddReminderViewModel
+import com.ybhgl.reminder.ui.add.ReminderSettingViewModel
+import com.ybhgl.reminder.ui.detail.DetailViewModel
+import com.ybhgl.reminder.ui.list.ReminderListViewModel
+import com.ybhgl.reminder.ui.settings.SettingsViewModel
+import com.ybhgl.reminder.ui.settings.BackupAndRestoreViewModel
+import com.ybhgl.reminder.ui.tag.TagManagementViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Reminder app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for BackupAndRestoreViewModel
+        initializer {
+            val app = reminderApplication()
+            BackupAndRestoreViewModel(
+                reminderRepository = app.container.reminderRepository,
+                tagRepository = app.container.tagRepository
+            )
+        }
+
+        // Initializer for ReminderSettingViewModel
+        initializer {
+            val application = reminderApplication()
+            ReminderSettingViewModel(
+                savedStateHandle = createSavedStateHandle(),
+                reminderRepository = application.container.reminderRepository
+            )
+        }
+
+        // Initializer for AddReminderViewModel
+        initializer {
+            val application = reminderApplication()
+            AddReminderViewModel(
+                reminderRepository = application.container.reminderRepository,
+                tagRepository = application.container.tagRepository,
+                savedStateHandle = createSavedStateHandle()
+            )
+        }
+
+        // Initializer for DetailViewModel
+        initializer {
+            DetailViewModel(
+                savedStateHandle = this.createSavedStateHandle(),
+                reminderRepository = reminderApplication().container.reminderRepository,
+                tagRepository = reminderApplication().container.tagRepository
+            )
+        }
+
+        // Initializer for ReminderListViewModel
+        initializer {
+            ReminderListViewModel(
+                reminderRepository = reminderApplication().container.reminderRepository,
+                tagRepository = reminderApplication().container.tagRepository
+            )
+        }
+
+        // Initializer for SettingsViewModel
+        initializer {
+            SettingsViewModel(reminderApplication().container.reminderRepository)
+        }
+
+        // Initializer for TagManagementViewModel
+        initializer {
+            TagManagementViewModel(reminderApplication().container.tagRepository)
+        }
+    }
+}
+
+/**
+ * Extension function to queries for Application object and returns an instance of
+ * ReminderApplication.
+ */
+fun CreationExtras.reminderApplication(): ReminderApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ReminderApplication)
