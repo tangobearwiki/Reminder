0736937430b0cf94c7d21939472b8f9f60ba5d60
Tangobear <tangobearwiki@gmail.com>
2026-08-15 23:50:18 +0800

    Add app/src/main/java/com/ybhgl/reminder/data/AppContainer.kt

---
+++ app/src/main/java/com/ybhgl/reminder/data/AppContainer.kt
@@ -0,0 +1,25 @@
+package com.ybhgl.reminder.data
+
+import android.content.Context
+
+/**
+ * App container for Dependency injection.
+ */
+interface AppContainer {
+    val reminderRepository: ReminderRepository
+    val tagRepository: TagRepository
+}
+
+/**
+ * [AppContainer] implementation that provides instance of [ReminderRepository]
+ */
+class DefaultAppContainer(private val context: Context) : AppContainer {
+    override val reminderRepository: ReminderRepository by lazy {
+        ReminderRepository(ReminderDatabase.getDatabase(context).reminderDao(), context)
+    }
+
+    override val tagRepository: TagRepository by lazy {
+        val database = ReminderDatabase.getDatabase(context)
+        TagRepository(database.tagDao(), database.reminderDao(), context)
+    }
+}
