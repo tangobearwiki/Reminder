66b0b293876e09396117993dd31c685c1f29ca82
Tangobear <tangobearwiki@gmail.com>
2026-08-16 08:08:57 +0800

    fix: resolve imports/params in WidgetConfigureActivity.kt

--- app/src/main/java/com/ybhgl/reminder/widget/WidgetConfigureActivity.kt
+++ app/src/main/java/com/ybhgl/reminder/widget/WidgetConfigureActivity.kt
@@ -8,9 +8,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.SystemBarStyle
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
